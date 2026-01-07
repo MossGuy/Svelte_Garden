96f334b2509ce90e3c82b0a741f2a5dd26e24a43
MossGuy <milanpallas@hotmail.nl>
2026-01-07 09:20:57 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<nav class="flex justify-center text-lg font-semibold border-b-2 pb-1 bg-teal-700">
+	<div class="flex gap-2 mr-auto px-2">
+		<a class="mr-5 hover:underline" href="/">Home</a>
+	</div>
+</nav>
+
+<main class="bg-gray-200 min-h-screen px-4 py-6">
+	{@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import svelteImage from '$lib/assets/Svelte_Logo.png';
+</script>
+
+<header class="flex justify-center items-center py-6 flex-col lg:flex-row">
+    <img class="w-40" src="{svelteImage}" alt="">
+    <div class="text-center">
+        <h1 class="text-4xl font-bold">Welcome to Svelte Garden!</h1>
+        <p class="text-lg">The place where I practice my Svelte.</p>
+        <p class="text-xs">Made in SvelteKit. Oficial documentation: <a target="new" class="text-orange-700" href="https://svelte.dev/">svelte.dev</a></p>
+    </div>
+</header>
